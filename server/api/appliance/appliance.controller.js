9eb49228126805f1f599718a36d6c223febcfbe1
namgunghyeon <namkunghyeon@namkunghyeonui-MacBook-Pro.local>
2015-04-22 21:12:23 +0900

    디바이스  GET /:email/device 유저가 등록한 디바이스 정보     query : email
     POST /:email/device 유저의 디바이스 등록
        query : email
        body : serial, deviceHash, label
    
    유저 어플라이언스
     GET /:email/appliances 유저가 등록한 어플라이언스 목록(에너지 스캔)
         query : email
    
     POST /:email/appliances 유저 어플라이언스 등록(에너지 스캔)
        query : email
        body : model, id(appliance_type_id), email, desc
    
    유저 기본 어플라이언스 종류
     GET /:email/applianceType 유저의 어플라이언스 종류(플러스를 눌러추가)
        query : email
    
     POST /:email/applianceType 어플라이언스 종류 추가(플러스를 눌러 추가 하는 부분)
        query : email
        body : appCode, desc
               appCode는 A5로 고정
    
     POST /:email/defaultUserAppliance 유저의 기본 어플라이언스 목록 추가(회원가입을 하고 자동으로 실행해야 하지만 한번만 실행하는 시점이 맞지 않아
             최초 1회 수동 실행)
        query : email
            자동으로 applianceType에 냉장고, 김치냉장고, 세탁기 전기 밥솥, 기타 추가
    
    스캔
     GET /:email/appliances/scan 스캔한 목록
        query : email
    
     POST /:email/appliances/scan 스캔 목록에 추가
        query : email
        body : start, end, totalUsage, id(appliance_id)
        option : mode1,mode2

--- server/api/appliance/appliance.controller.js
+++ server/api/appliance/appliance.controller.js
@@ -6,16 +6,36 @@ var mysql = require('mysql');
 var connection = mysql.createConnection(env.MYSQL);
 
 // Get list of appliances
+
+
+function selectQueryUserApplianceTypeByEmail(queryInfos) {
+    var sql = 'SELECT A.id, B.code, A.desc, B.name FROM user_appliance_type AS A INNER JOIN appliance_code AS B' +
+            '  ON A.appliance_code = B.code' +
+            ' WHERE user_email = \''+ queryInfos.email +'\'';
+
+    return sql;
+}
+
+
 exports.getApplianceTypeList = function(req, res) {
+    var email = req.params.email || null;
 
-    var sql = 'SELECT * FROM appliance_code';
+    if (email === null ) {
+        res.send('not fount email');
+        return;
+    }
 
-      connection.query(sql, function (err, results) {
+    var queryInfos = {
+            email : email,
+        },
+    sql = selectQueryUserApplianceTypeByEmail(queryInfos);
+
+    connection.query(sql, function (err, results) {
 
         if (err) {
           throw err;
         }
         res.json(results);
 
-      });
+    });
 };
--- server/api/appliance/appliance.spec.js
+++ server/api/appliance/appliance.spec.js
@@ -8,7 +8,7 @@ describe('GET /api/appliances', function() {
 
   it('should respond with JSON array', function(done) {
     request(app)
-      .get('/y')
+      .get('/api/appliances')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
